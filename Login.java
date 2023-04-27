3cd67ee3c4d78425148492da7f5343a55ab11cd6
BOTC3PO <javier.perez1314@gmail.com>
2023-04-27 15:02:21 -0300

    login update

---
+++ Login.java
@@ -0,0 +1,52 @@
+package main;
+
+public class Login {
+	/*
+	String dat= "dat";
+	char gi=dat.charAt(0);
+	*/
+	final char tipo[] = {'c','e'};
+	final char empleado[]= {'v','a','r','i'};
+	final String tipo_de_usuario[]={"cliente","ventas","administrador","restauracion","reparacion"};
+	private boolean resul; 
+	
+
+	public Login(boolean resul) {
+		this.resul = resul;
+	}
+
+	public boolean isResul() {
+		return resul;
+	}
+
+	public void setResul(boolean resul) {
+		this.resul = resul;
+	}
+
+
+
+	public String d(String value) {
+	if (value.charAt(0)==tipo[0]) {
+		return tipo_de_usuario[0];
+	}
+	if (value.charAt(0)==tipo[1]) {
+		
+		if (value.charAt(1)==empleado[0]) {
+			return tipo_de_usuario[1];
+		}	
+		if (value.charAt(1)==empleado[1]) {
+			return tipo_de_usuario[2];
+		}	
+		if (value.charAt(1)==empleado[2]) {
+			return tipo_de_usuario[3];
+		}	
+		if (value.charAt(1)==empleado[3]) {
+			return tipo_de_usuario[4];
+		}	
+	}
+	
+	return "0"; 
+	}
+	
+
+}
