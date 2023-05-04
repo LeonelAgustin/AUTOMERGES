3f4870e78ba17e35daf17c236fbeacea6385510a
BOTC3PO <74213407+BOTC3PO@users.noreply.github.com>
2023-05-04 01:05:19 -0300

    ventas 50%
    creado
    auto , pieza
    solo creadas para el futuro
    
    update
    vendedor ,ventas
    vendedor se actualizo el tipo de cuenta para que el administrador cambie el tipo de usuario falta si esta despedido o no
    ventas falta crear la lista de transacciones y el resumen

--- ventas/Logica/Vendedor.java
+++ ventas/Logica/Vendedor.java
@@ -8,6 +8,14 @@ public class Vendedor extends Persona {
 		super(nombre, apellido, dni , id, contrasena);
 		this.tipo_cuenta="Vendedor";
 	}
+	
+	public String getTipo_cuenta() {
+		return tipo_cuenta;
+	}
+	//solo puede modificar administrador el tipo de cuenta
+	public void setTipo_cuenta(String tipo_cuenta) {
+		this.tipo_cuenta = tipo_cuenta;
+	}
 
 	
 
--- ventas/Logica/Ventas.java
+++ ventas/Logica/Ventas.java
@@ -3,7 +3,47 @@ package Logica;
 public class Ventas {
 
     private Persona persona;
-
-    
-    
+    private Pieza pieza;
+    private Auto auto;
+    final public String transaccion[] ={"compra","venta","reparacion","restauracion"};
+    private String tipo_transaccion;
+    //
+	public Ventas(Persona persona, Auto auto,int tipo) {
+		super();
+		this.persona = persona;
+		this.auto = auto;
+		this.tipo_transaccion=transaccion[tipo];
+	}
+	public Ventas(Persona persona, Pieza pieza,int tipo) {
+		super();
+		this.persona = persona;
+		this.pieza = pieza;
+		this.tipo_transaccion=transaccion[tipo];
+	}
+	public Persona getPersona() {
+		return persona;
+	}
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+	public Auto getAuto() {
+		return auto;
+	}
+	public void setAuto(Auto auto) {
+		this.auto = auto;
+	}
+	public Pieza getPieza() {
+		return pieza;
+	}
+	public void setPieza(Pieza pieza) {
+		this.pieza = pieza;
+	}
+	public String getTipo_transaccion() {
+		return tipo_transaccion;
+	}
+	public void setTipo_transaccion(String tipo_transaccion) {
+		this.tipo_transaccion = tipo_transaccion;
+	}
+    	
+   
 }
