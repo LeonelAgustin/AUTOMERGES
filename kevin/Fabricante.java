6e5045380a9e9ffeeee07076e494ec19c697e62d
Stone17KP <113469320+Stone17KP@users.noreply.github.com>
2023-05-08 12:22:11 -0300

    COMMIT FIN

--- kevin/Fabricante.java
+++ kevin/Fabricante.java
@@ -1,5 +1,9 @@
 package automerges;
 
+import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
+
 public class Fabricante {
 	private int id_frabicante;
 	private String claveF;
@@ -31,5 +35,34 @@ public class Fabricante {
 		return "Fabricante [id_frabicante=" + id_frabicante + ", claveF=" + claveF + "]";
 	}
 	
+	public static void FabricarPieza() {
+		int sele;
+		Pieza p=new Pieza();
+		JOptionPane.showMessageDialog(null, "Bienvenido fabricante...");
+		do {
+			int selec=Integer.parseInt(JOptionPane.showInputDialog("Elija una de estas opciones de fabricacion \n1.Iniciar Proceso de fabricacion \n2.Parar Proceso \n3.Reanudar Proceso\n4.Borrar proceso \n5.Ver lista de procesos \nOtro numero para Salir"));
+			
+			switch (selec) {
+			case 1:
+				p.IniciarProceso();
+				break;	
+			case 2:
+				p.PararProceso();
+				break;
+			case 3:
+				p.ReanudarProceso();
+				break;
+			case 4:
+				p.BorrarProceso();
+				break;
+			case 5:
+				p.Lista();
+				break;
+			default:
+				break;
+			}
+			sele = Integer.parseInt(JOptionPane.showInputDialog("Ingrese \n1Si quiere volver al menu \nOtro numero, salir"));
+		}while(sele==1);
+	}
 	
 }
--- kevin/Pieza.java
+++ kevin/Pieza.java
@@ -6,23 +6,34 @@ import javax.swing.JOptionPane;
 
 public class Pieza {
 	private int numero_de_pieza;
+	private String nombre;
 	private double precio;
-	private boolean importada;
-	private LinkedList<Pieza> Clasificados = new LinkedList<Pieza>();
+	private String importada;
+	private LinkedList<Pieza> Piezas = new LinkedList<Pieza>();
 	
-	public Pieza(int numero_de_pieza, double precio, boolean importada) {
+	
+	public Pieza(int numero_de_pieza, String nombre, double precio, String importada) {
 		super();
 		this.numero_de_pieza = numero_de_pieza;
+		this.nombre = nombre;
 		this.precio = precio;
 		this.importada = importada;
 	}
-
+	
 	public int getNumero_de_pieza() {
 		return numero_de_pieza;
 	}
 
 	public void setNumero_de_pieza(int numero_de_pieza) {
 		this.numero_de_pieza = numero_de_pieza;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public double getPrecio() {
@@ -33,45 +44,98 @@ public class Pieza {
 		this.precio = precio;
 	}
 
-	public boolean isImportada() {
+	public String getImportada() {
 		return importada;
 	}
 
-	public void setImportada(boolean importada) {
+	public void setImportada(String importada) {
 		this.importada = importada;
+	}
+	public Pieza() {
+		this.Piezas = new LinkedList<Pieza>();    
+	}
+	
+	public LinkedList<Pieza> getPiezas() {
+		return Piezas;
 	}
 
 	@Override
 	public String toString() {
-		return "Pieza [numero_de_pieza=" + numero_de_pieza + ", precio=" + precio + ", importada=" + importada + "]";
+		return "Numero de Pieza: "+numero_de_pieza+"\nNombre: "+nombre+"\nPrecio:"+precio+"\nImportado: "+importada;
 	}
-	
-	public boolean IniciarProceso(){
-		int agregap=Integer.parseInt(JOptionPane.showInputDialog("ingrese "));
-		{
-			
-			return true;
-		}else {
+	private LinkedList<Pieza> PiezasPausadas = new LinkedList<Pieza>();
+	public String IniciarProceso(){
+		int sel;
+		int num=+1;
+		do {
+		double precio=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio"));
+		String nom = JOptionPane.showInputDialog("Ingrese el nombre de la pieza");
+		String imp="no";
+		
+		 Pieza pieza1 = new Pieza (num++,nom,precio,imp);
+		 Piezas.add(pieza1);
+		
+		sel = Integer.parseInt(JOptionPane.showInputDialog("Ingrese \n1Si quiere agregar otra pieza \n2 Voler al menu"));
+		} while (sel==1);
+		return "";
+	}
+	public boolean PararProceso(){
+		JOptionPane.showMessageDialog(null, Piezas);
+		int piez=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de pieza, del cual desea parar el proceso"));
+		if (Piezas.isEmpty()) {
+			JOptionPane.showMessageDialog(null,"La lista esta vacia");
 			return false;
+		}else {
+			 for (Pieza elemento : Piezas)
+			        if(elemento.getNumero_de_pieza()==piez) {
+			        	System.out.println("Paro el proceso de la pieza: "+elemento.getNumero_de_pieza()); 
+			        	PiezasPausadas.add(Piezas.get(piez));
+			    		Piezas.remove(piez);
+			    		
+		}
+		}
+		
+			return true;
+	}
+	public boolean ReanudarProceso(){
+		JOptionPane.showMessageDialog(null, PiezasPausadas);
+		int pie=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de pieza, del cual desea reanudar el proceso"));
+		if (PiezasPausadas.isEmpty()) {
+			JOptionPane.showMessageDialog(null,"La lista esta vacia");
+			return false;
+		}else {
+			 for (Pieza elemento : PiezasPausadas)
+			        if(elemento.getNumero_de_pieza()==pie) {
+			        	System.out.println("Reanudo la pieza: "+elemento.getNumero_de_pieza()); 
+			        	Piezas.add(PiezasPausadas.get(pie));
+			        	PiezasPausadas.remove(pie);
+		}
+		}
+			return true;
+	}      	
+        
+	public boolean BorrarProceso(){
+		int piee=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de pieza para borrar el proceso"));
+		if (Piezas.isEmpty()) {
+			JOptionPane.showMessageDialog(null,"La lista esta vacia");
+			return false;
+		}else {
+			 for (Pieza elemento : Piezas)
+			        if(elemento.getNumero_de_pieza()==piee) {
+			        	System.out.println("removio el elemento: "+elemento.getNumero_de_pieza()); 
+			        	Piezas.remove(piee);
+		}
+			 return true;
 		}
 	}
-	public boolean PararProceso(Pieza pieza1){
-		if(1==1)
-		{
-			return true;
+	public String Lista() {
+		if (Piezas.isEmpty()) {
+			JOptionPane.showMessageDialog(null,"La lista esta vacia");
 		}else {
-			return false;
+		JOptionPane.showMessageDialog(null, Piezas);
 		}
+		return "";
+		
 	}
-	public boolean BorrarProceso(Pieza pieza1){
-		if(1==1)
-		{
-			return true;
-		}else {
-			return false;
-		}
+
 	}
-	}
-	
-	
-}
